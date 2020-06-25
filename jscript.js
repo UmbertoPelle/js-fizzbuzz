418cea674e6a2dd2e9c06f3095bc8c572d30c740
Umberto <umbertopellegrino96@gmail.com>
2020-06-25 16:31:38 +0200

    font awesome

--- jscript.js
+++ jscript.js
@@ -4,12 +4,11 @@ var list = document.getElementById("buzzList");
 for (var i = 0; i < 101; i++) {
   // Per i numeri che sono sia multipli di 3 che di 5 stampi "FizzBuzz" al posto del numero.
   if (i % 3 == 0 && i % 5 == 0) {
-  //  list.innerHTML += "<li class="+"green"+">" + "FizzBuzz" + "</li>"
-    list.innerHTML += "<li>" + "<i class="+"fa-facebook-messenger"+"></i>" + "</li>"
-
+    list.innerHTML += "<li>" + "<i class=\"fas fa-beer\"></i>" + "</li>"
     // Per i multipli di 3 stampi “Fizz” al posto del numero.
   }else if (i % 3 == 0) {
     list.innerHTML += "<li class="+"blue"+">" + "Fizz" + "</li>"
+    list.innerHTML += "<li>" + "<i class=\"fas fa-beer\"></i>" + "</li>"
     // Per i multipli di 5 stampi "Buzz" al posto del numero.
   }else if (i % 5 == 0) {
     list.innerHTML += "<li class="+"red"+">" + "Buzz" + "</li>"
